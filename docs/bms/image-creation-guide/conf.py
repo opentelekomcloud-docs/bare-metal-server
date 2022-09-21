c37439ae8a567c8e94b1350b68f332cb217a73f7
Artem Goncharov <Artem.goncharov@gmail.com>
2022-09-21 09:45:30 +0200

    Update conf.py file (#3)
    Update conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: Vladimir Hasko <vladimirhasko@gmail.com>

--- docs/bms/image-creation-guide/conf.py
+++ docs/bms/image-creation-guide/conf.py
@@ -80,7 +80,7 @@ html_theme_options = {
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 
-html_title = "Bare Metal Server - User Guide"
+html_title = "Bare Metal Server - Private Image Creation Guide"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -91,7 +91,7 @@ html_static_path = ['_static']
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
 ('index',
-     'bms-umn.tex',
-     u'Bare Metal Server - User Guide',
+     'bms-image-creation-guide.tex',
+     u'Bare Metal Server - Private Image Creation Guide',
      u'OpenTelekomCloud', 'manual'),
 ]
