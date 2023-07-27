ce76ddd4633b64393958e7024f3bd0a94bd68baa
Sebastian Gode <sebastian.gode@telekom.de>
2023-07-27 13:03:42 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,6 +29,17 @@ project = 'Bare Metal Server'
 otcdocs_repo_name = 'opentelekomcloud-docs/bare-metal-server'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = ''
+otcdocs_doc_link = ''
+otcdocs_doc_title = ''
+otcdocs_doc_type = ''
+otcdocs_service_category = 'compute'
+otcdocs_service_title = 'Bare Metal Server'
+otcdocs_service_type = 'bms'
+otcdocs_search_environment = 'hc_de'
+otcdocs_search_url = "https://opensearch.eco.tsi-dev.otc-service.com/"
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
